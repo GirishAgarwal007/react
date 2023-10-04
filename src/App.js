b0dcad07f44209ffa0a488a26e21125badbb2895
Ubuntu <ubuntu@ip-172-31-85-8.ec2.internal>
2023-10-04 09:36:52 +0000

    new commit

--- src/App.js
+++ src/App.js
@@ -8,7 +8,6 @@ function App() {
 	  <h1> Welcome to the OG </h1> 
 	  <h1> This Project is handled by OG </h1>
 	  <h1> Welcome to the OG Family </h1>
-	  <h1> Welcome to Noida </h1>
 	  <h1> This is my first Pipeline </h1>
           <h1> New code generated <h1>
 	  </div>
