0bb03f32d0b4fc1e3bd9c5ebe12aecc0361fbf39
Ubuntu <ubuntu@ip-172-31-85-8.ec2.internal>
2023-10-04 11:00:08 +0000

    new commit

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
   	<h1>Hello World! </h1>
 	  <h1> Welcome to the OG Family </h1>
 	  <h1> This is Jenkins Pipeline Project </h1>
+	  <h1> Scripted Pipeline </h1>
 	  </div>
   );
 }
