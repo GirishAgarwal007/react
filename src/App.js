037020278d07a31175c29588a30599305e1d34ac
Girish Agarwal <108732661+GirishAgarwal007@users.noreply.github.com>
2023-10-11 17:46:26 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
   	<h1>Hello World! </h1>
-	  <h1> Welcome to the OG Family </h1>
+	  <h1> Welcome to the Noida </h1>
 	  <h1> This is Jenkins Pipeline Project </h1>
 	  <h1> Scripted Pipeline </h1>
 	  <h1> Hello World! again </h1>
